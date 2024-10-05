16370962bbae269755179b43bca0bb7c5a02b521
Santhini16 <santhinimuthu16@gmail.com>
2024-10-05 21:28:29 +0530

    Create year.c

---
+++ year.c
@@ -0,0 +1,13 @@
+int main()
+{
+    int year;
+    scanf("%d",&year);
+    if(year%4==0)
+    {
+        printf("the yaer %d is a leap year",year);
+    }
+    else
+    {
+        printf("the year %d is not a leap year",year);
+    }
+}
