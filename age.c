0343a59af37c5e1ef0a34fede7685390e76fd995
Santhini16 <santhinimuthu16@gmail.com>
2024-10-05 21:10:28 +0530

    Create age.c

---
+++ age.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   int age;
+   scanf("%d",&age);
+   if(age>=18)
+   {
+   printf("the person is eligible for voting" );
+   }
+   else
+   {
+       printf("the person is not eligible for voting");
+   }
+}
