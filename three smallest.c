53e35ef69191a84036bb5aa554fe049daf600357
Santhini16 <santhinimuthu16@gmail.com>
2024-10-06 11:18:16 +0530

    Create three smallest.c

---
+++ three smallest.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a<b)
+    {
+        if(a<c)
+        {
+            printf("the number %d is smallest among the other two numbers",a);
+        }
+    }
+    else if(b<c)
+    {
+        if(b<a)
+        {
+            printf("the number %d is smallest among the other two numbers",b);
+        }
+    }
+    else if(c<a)
+    {
+        if(c<b)
+        {
+            printf("the number %d is smallest among the other two numbers",c);
+        }
+    }
+}
+  
